fd565b5af42511539b200747709548f42b9a38e2
KaranJanjua <karanbirjanjua@gmail.com>
2018-09-08 05:00:33 +0930

    Exit balance
    fixed the exit balance which gives the total of the purchases made at the end..

--- source/CashRegister.java
+++ source/CashRegister.java
@@ -4,39 +4,41 @@ public class CashRegister
 {
 	public static void main(String[] args)
 	{
-		String s, Name_item,Cost_item,Amount_tendered,Purchase_balance,exit_balance=null;
+		String s, Name_item,Cost_item,Amount_tendered,Purchase_balance;
 		double balance;
 
 		Scanner input = new Scanner(System.in);
 
 		String  ch;
 
-		System.out.print("Please enter the cash register's float: ");
+		System.out.print("Please enter the cash register's float: $");
 		s = input.nextLine();
 		balance = Double.parseDouble(s);
 
 		System.out.print("Would you like to enter the item [y/n]? ");;
 		ch=input.nextLine();
 
-		exit_balance=Double.toString(balance);
+		Double exit_balance=balance ;
+		int item_count=0;
 
 		while(ch.equalsIgnoreCase("y") ){
 
 			System.out.print("Please enter the item's name: ");
 			Name_item = input.nextLine();
 
-			System.out.print("Please enter the item's cost: ");
+			System.out.print("Please enter the item's cost: $");
 			Cost_item = input.nextLine();
 
 			Transaction trans = new Transaction(Name_item, Double.parseDouble(Cost_item));
 
-			System.out.print("Please enter the cash amount tendered: ");
+			System.out.print("Please enter the cash amount tendered: $");
 			Amount_tendered = input.nextLine();
 			Purchase_balance = Double.toString(Double.parseDouble(Amount_tendered) - trans.getCost());
 
-			System.out.println("Purchase Balance = " + Purchase_balance);
+			System.out.println("Purchase Balance = $" + Purchase_balance);
 
-			exit_balance = exit_balance + Double.toString(trans.getCost());
+			exit_balance = (exit_balance) + (trans.getCost());
+			item_count=item_count+1;
 
 
 
@@ -45,7 +47,7 @@ public class CashRegister
 		}
 
 
-
+		System.out.println("Number of items purchased: " + item_count);
 		System.out.println("Balance of the Cash Register: " + exit_balance);
 
 	}
